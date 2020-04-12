20396fd31d69e8953293fe2eb9f958c336134855
tata-ost17 <tata.ost17@gmail.com>
2020-04-12 21:06:15 +0300

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,40 @@
 $(function(){
 
-    $(".rate-star").rateYo({
+    $('.rate-star').rateYo({
       rating: 5,
-      starWidth: "12px",
+      starWidth: '12px',
       readOnly: true
     });
 
-    $(".prouct-slider__inner").slick({
+    $('.product-slider__inner').slick({
         dots: true,
         arrows: false,
         slidesToShow: 4,
-        slidesToScroll: 4
-
-
-
+        slidesToScroll: 4,
     });
-    var mixer = mixitup(".products__inner-box");
+
+     $('.js-range-slider').ionRangeSlider({
+       type: 'double',
+       min: 0,
+       max: 1000,
+       from: 0,
+       to: 600,
+       prefix: '$',
+     });
+
+     $('.icon-th-list').on('click', function(){
+       $('.product-page__items .product__item').addClass('list');
+       $('.icon-th-list').addClass('active');
+       $('.icon-th-large').removeClass('active');
+
+     });
+     $('.icon-th-large').on('click', function (){
+       $('.product-page__items .product__item').removeClass('list');
+         $('.icon-th-large').addClass('active');
+         $('.icon-th-list').removeClass('active');
+     });
+
+     
+    var mixer = mixitup('.products__inner-box');
 
 });
